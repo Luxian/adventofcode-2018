fc5a30ed815b4ba59d801482254ae99cde5c875f
Lucian NEAG <luxian.m@gmail.com>
2018-12-11 22:43:37 +0100

    Finish challenge 7

--- 07.php
+++ 07.php
@@ -3,49 +3,104 @@
 $input = file_get_contents(__DIR__ . '/07-input.txt');
 $lines = explode("\n", trim($input));
 $steps = [];
+$number_of_workers = 5;
+$solution = [];
+$seconds = 0;
 
 // Create list of steps from input
 foreach($lines as $line) {
     $step1 = $line[5];
     if (!isset($steps[$step1]['step'])) {
-        $steps[$step1]['step'] = $step1;
-        $steps[$step1]['before'] = [];
-        $steps[$step1]['depends'] = [];
+        $steps[$step1] = get_initial_step_structure($step1);
     }
 
     $step2 = $line[36];
     if (!isset($steps[$step2]['step'])) {
-        $steps[$step2]['step'] = $step2;
-        $steps[$step2]['before'] = [];
-        $steps[$step2]['depends'] = [];
+        $steps[$step2] = get_initial_step_structure($step2);
     }
 
     $steps[$step1]['before'][] = $step2;
     $steps[$step2]['depends'][] = $step1;
 }
 
-$steps_left = $steps;
-$solution = [];
-while(count($steps_left)) {
-    $valid_next_steps = [];
+while(count($steps)) {
+    $steps_that_can_be_worked_on = [];
 
-    foreach($steps_left as $step_left) {
-        $ready = count(array_diff($step_left['depends'], $solution)) === 0;
+    // Find steps that can be worked on
+    foreach($steps as $step) {
+        $ready = count(array_diff($step['depends'], $solution)) === 0;
         if ($ready) {
-            $valid_next_steps[] = $step_left['step'];
+            $steps_that_can_be_worked_on[] = $step['step'];
         }
     }
+    unset($step);
 
-    if (count($valid_next_steps) === 0 && count($steps_left)) {
-        die('error' . __LINE__);
-    }
-    if (count($valid_next_steps) > 1) {
-        sort($valid_next_steps);
+    if (count($steps_that_can_be_worked_on) === 0 && count($steps)) {
+        die('Error: could not find solution');
     }
 
-    $next_step = reset($valid_next_steps);
-    unset($steps_left[$next_step]);
-    $solution[] = $next_step;
+    // Decide which steps can be worked on this second
+    $steps_worked_this_second = 0;
+    uasort(
+        $steps_that_can_be_worked_on,
+        function (string $step1, string $step2) use ($steps) {
+            // steps completed are not not being worked on
+            if ($steps[$step1]['cost'] === 0){
+              return $steps[$step2]['cost'] === 0 ? 0 : 1;
+            }
+            if ($steps[$step2]['cost'] === 0) {
+                return -1;
+            }
+            // if one is already started work on that first
+            if ($steps[$step1]['started'] !== $steps[$step2]['started']) {
+                return $steps[$step1]['started'] ? -1 : 1;
+            }
+
+            return strcmp($steps[$step1]['step'], $steps[$step2]['step']);
+        }
+    );
+    foreach($steps_that_can_be_worked_on as $step) {
+        if ($steps[$step]['cost']) {
+            $steps[$step]['cost']--;
+            $steps[$step]['started'] = true;
+            $steps_worked_this_second++;
+
+            if ($steps_worked_this_second === $number_of_workers) {
+                break;
+            }
+        }
+    }
+    unset($step);
+    $seconds++;
+
+    $steps_that_are_completed = array_filter(
+        $steps_that_can_be_worked_on,
+        function(string $step) use ($steps) {
+            return $steps[$step]['cost'] === 0;
+        }
+    );
+    if (count($steps_that_are_completed) > 1) {
+        sort($steps_that_are_completed);
+    }
+
+    $next_step = reset($steps_that_are_completed);
+    if ($steps[$next_step]['cost'] === 0) {
+        unset($steps[$next_step]);
+        $solution[] = $next_step;
+    }
 }
 
 echo 'Solution: ' . implode('', $solution) . PHP_EOL;
+echo 'Duration: ' . $seconds;
+
+// -----
+function get_initial_step_structure(string $step): array
+{
+    $ascii_to_cost_offset = ord('A') - 1;
+    return [
+        'step' => $step,
+        'depends' => [],
+        'cost' => 60 + (ord($step) - $ascii_to_cost_offset),
+        'started' => false,
+    ];
+}
